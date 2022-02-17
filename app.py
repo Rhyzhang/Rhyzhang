528029ceed4273b66863a549023601d7a3963d23
Ryan <ryanz22@nycstudents.net>
2022-02-17 10:22:19 -0500

    Update

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import json
 
+import streamlit as st
 from streamlit_echarts import JsCode, st_echarts
 
 
 def render_disk_usage():
+    """Tree Map"""
     with open("./me.tree.json", "r") as f:
         diskData = json.loads(f.read())
 
@@ -35,4 +37,61 @@ def render_disk_usage():
     }
     st_echarts(option, height="500px")
 
+
+def render_basic_tree():
+    """Basic Tree diagram"""
+    with open("./flare.json", "r") as f:
+        data = json.loads(f.read())
+
+    for idx, _ in enumerate(data["children"]):
+        data["children"][idx]["collapsed"] = idx % 2 == 0
+
+    option = {
+        "tooltip": {"trigger": "item", "triggerOn": "mousemove"},
+        "series": [
+            {
+                "type": "tree",
+                "data": [data],
+                "top": "1%",
+                "left": "7%",
+                "bottom": "1%",
+                "right": "20%",
+                "symbolSize": 7,
+                "label": {
+                    "position": "left",
+                    "verticalAlign": "middle",
+                    "align": "right",
+                    "fontSize": 9,
+                },
+                "leaves": {
+                    "label": {
+                        "position": "right",
+                        "verticalAlign": "middle",
+                        "align": "left",
+                    }
+                },
+                "emphasis": {"focus": "descendant"},
+                "expandAndCollapse": True,
+                "animationDuration": 550,
+                "animationDurationUpdate": 750,
+            }
+        ],
+    }
+    st_echarts(option, height="500px")
+
+
+
+
+# Tree map
 render_disk_usage()
+
+st.header("Idenity Projec[")
+
+
+
+# # Tree
+# render_basic_tree()
+
+
+
+
